0b5d1efb0e48dfbdea3037a310fa3e933921543e
peccu <peccul@gmail.com>
2023-01-13 06:23:42 +0000

    merge completion conv

--- dot_bash/conf/completion.sh
+++ dot_bash/conf/completion.sh
@@ -1,12 +1,15 @@
-# -*- shell-script -*-
-# enable bash completion in interactive shells
+# -*- coding:utf-8; mode:shell-script  -*-
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
 if ! shopt -oq posix; then
-    if [ -f /usr/share/bash-completion/bash_completion ]; then
-        . /usr/share/bash-completion/bash_completion
-    elif [ -f /etc/bash_completion ]; then
-        . /etc/bash_completion
-    fi
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
 fi
+
 [ -f ~/.bash/.bash_completion ] && source ~/.bash/.bash_completion
 
 # https://serverfault.com/a/831184
