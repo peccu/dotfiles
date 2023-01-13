570ef715a029889140cf4e3d6099e0225ad0723f
peccu <peccul@gmail.com>
2023-01-13 06:47:27 +0000

    add atuin conf

---
+++ dot_bash/conf/atuin.sh
@@ -0,0 +1,9 @@
+# -*- coding:utf-8; mode:shell-script  -*-
+# # cool history management
+# # maybe not work in bash (only importing history??)
+# # curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
+# if exists atuin
+# then
+#     [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
+#     eval "$(atuin init bash)"
+# fi
