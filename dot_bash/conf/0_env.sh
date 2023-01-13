77d8f9a434a450c76460a11c24690aa8d61256de
peccu <peccul@gmail.com>
2023-01-13 06:51:17 +0000

    add some env conf

---
+++ dot_bash/conf/0_env.sh
@@ -0,0 +1,3 @@
+# -*- shell-script -*-
+export LC_ALL=en_US.UTF-8
+export TERM=xterm-256color
---
+++ dot_bash/conf/0_path.sh
@@ -0,0 +1,3 @@
+# -*- shell-script -*-
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
+PATH=~/.local/bin:$PATH
