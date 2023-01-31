8c467baeb176c3797fb92b9b90dc40741ffb4f89
peccu <peccul@gmail.com>
2023-01-31 04:17:39 +0000

    merge bash configs

--- dot_bash/conf/history.sh
+++ dot_bash/conf/history.sh
@@ -1,11 +1,17 @@
 # -*- coding:utf-8; mode:shell-script  -*-
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
-HISTCONTROL=ignoreboth
+export HISTCONTROL=ignoreboth
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=
-HISTFILESIZE=
+export HISTSIZE=
+export HISTFILESIZE=
+export HISTTIMEFORMAT="%F %T "
+
+# This will be overwrite by zoxide (unshift __zoxide_hook)
+# append, clear, restore
+# export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+export PROMPT_COMMAND="history -a; history -c; history -r"
