1a7f60c1e6a3d19e6c6fd07cc99386063d8e2b9a
peccu <peccul@gmail.com>
2023-02-12 14:22:09 +0000

    add some comment for bash history

--- dot_bash/conf/history.sh
+++ dot_bash/conf/history.sh
@@ -9,9 +9,13 @@ shopt -s histappend
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 export HISTSIZE=
 export HISTFILESIZE=
+# date format
 export HISTTIMEFORMAT="%F %T "
+# export HISTTIMEFORMAT="%y/%m/%d-%T "
 
 # This will be overwrite by zoxide (unshift __zoxide_hook)
-# append, clear, restore
 # export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+# history -a : append immediately
+# history -c : clear current session history
+# history -r : read history from file
 export PROMPT_COMMAND="history -a; history -c; history -r"
