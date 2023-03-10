6dea73c4a45e11623e52b92ac195bdfe95d7c719
peccu <peccul@gmail.com>
2023-03-10 12:27:29 +0000

    implement snippet manager

---
+++ dot_bash/conf/snippet-manager.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+export SNIP_DIR=~/.local/share/snip
+
+# first line is description
+# the lasting lines are the snippet
+
+function snip(){
+    (
+        cd $SNIP_DIR
+        for i in snip*
+        do
+            echo -n "$i,"
+            head -n 1 -q $i
+        done \
+            | fzf \
+                  --delimiter , \
+                  --with-nth 2.. \
+                  --preview 'tail -n +2 {1} | bat -n --color=always --file-name {1}' \
+                  --preview-window up,60% \
+                  --info inline \
+                  --bind 'enter:become(cat {1} | copy)'
+    )
+}
+
+function snip-add(){
+    (
+        EDITOR='emacs -Q -nw'
+        snip-checkdirty || return 1
+        read -p "Description: " desc
+        read -p "Extension: " ext
+        read -p "Snippet (leave empty to open EDITOR): " code
+        tmpfile=$(mktemp -t snip.XXXXXX --suffix=.$ext)
+        if [ -z "$code" ]
+        then
+            $EDITOR $tmpfile
+        else
+            printf "$code\n" > $tmpfile
+        fi
+        snip-sync
+        cd $SNIP_DIR
+        nextnumber=$((1+ $(ls snip* | cut -d. -f1 | cut -d_ -f2 | sort -n | tail -1)))
+        cat <(echo "$desc") $tmpfile > snip_$nextnumber.$ext
+        rm $tmpfile
+        git add snip_$nextnumber.$ext
+        git commit -m "add snippet: $desc"
+        git push origin
+    )
+}
+alias sa=snip-add
+
+function snip-checkdirty(){
+    (
+        cd $SNIP_DIR
+        git status -s | grep snip \
+            && (
+            echo
+            echo directory is dirty. please check
+            return 1
+        )
+        return 0
+    )
+}
+
+function snip-sync(){
+    (
+        cd $SNIP_DIR
+        git fetch origin
+        git reset --hard origin/main > /dev/null
+    )
+}
