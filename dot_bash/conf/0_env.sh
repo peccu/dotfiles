29a57786cd4a6102fc73d0cc63af1c8ad74ff625
peccu <peccul@gmail.com>
2024-10-25 09:04:12 +0000

    add GCM_CREDENTIAL_STORE

--- dot_bash/conf/0_env.sh
+++ dot_bash/conf/0_env.sh
@@ -3,3 +3,5 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export TERM=xterm-256color
 export EDITOR='_editor () { emacs -Q -nw "$@"|| zile "$@"|| nano "$@" || vi "$@"; } ; _editor'
+# needs git-credential-manager github login
+export GCM_CREDENTIAL_STORE=plaintext
