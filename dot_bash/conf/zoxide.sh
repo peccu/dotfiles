ec1ac58a91a928a79194460a46aec39415ef9937
peccu <peccul@gmail.com>
2026-02-12 21:30:13 +0900

    update zoxide conf

--- dot_bash/conf/zoxide.sh
+++ dot_bash/conf/zoxide.sh
@@ -3,10 +3,10 @@ if exists zoxide
 then
     # print the matched directory before navigationg to it
     export _ZO_ECHO=1
-    eval "$(zoxide init bash)"
-    alias j=z
+    eval "$(zoxide init bash --cmd j)"
+    # alias j=z
     # zi interactive selection
-    # zq query only not cd
-    # za add path
-    # zr remove path
+    # ? zq query only not cd
+    # ? za add path
+    # ? zr remove path
 fi
