53f467687e3c0dc3c3e5e32f1ba923f2ce361043
peccu <peccul@gmail.com>
2026-03-04 00:47:31 +0900

    update path config

--- dot_bash/conf/0_path.sh
+++ dot_bash/conf/0_path.sh
@@ -1,3 +1,13 @@
 # -*- shell-script -*-
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
-PATH=~/.local/bin:$PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
+[ -d ~/.local/bin ] && export PATH=$HOME/.local/bin:$PATH
+export PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
+# psql, pg_dump
+[ -d /opt/homebrew/opt/libpq/bin ] && export PATH=/opt/homebrew/opt/libpq/bin:$PATH
+# Python
+[ -d ~/Library/Python/3.9/bin ] && export PATH="$PATH:$HOME/Library/Python/3.9/bin"
+
+[ -d /opt/homebrew/opt/ruby/bin ] && export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+# [ -d /opt/homebrew/opt/ruby@3.1/bin ] && export PATH="/opt/homebrew/opt/ruby@3.1/bin:$PATH"
+[ -d /opt/homebrew/opt/ruby@3.4/bin ] && export PATH="/opt/homebrew/opt/ruby@3.4/bin:$PATH"
+
