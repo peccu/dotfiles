59dde7500c024163fada364a605ff94b98db630e
peccu <peccul@gmail.com>
2026-03-08 15:14:09 +0900

    update skim options

--- dot_zsh/conf/skim.sh
+++ dot_zsh/conf/skim.sh
@@ -1,2 +1,8 @@
-type sk >/dev/null \
-    && source <(sk --shell zsh)
+if type sk >/dev/null
+then
+    source <(sk --shell zsh)
+    # activate RegExp mode with C-r
+    export SKIM_CTRL_R_OPTS="--bind=ctrl-r:rotate-mode"
+    # sort by score
+    export SKIM_DEFAULT_OPTIONS="--tiebreak score,index"
+fi
