414fb6073ee424bf172a191e5e564158a2d0c2d0
peccu <peccul@gmail.com>
2026-03-15 15:19:54 +0900

    add shortcut keys to update tab and window name for WezTerm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -16,4 +16,35 @@ config.font_size = 14.0
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 
+-- Key bindings
+local act = wezterm.action
+config.keys = {
+  -- Rename tab title via input prompt
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new tab title',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+  -- Rename window title via input prompt
+  {
+    key = 'W',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new window title',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          pane:inject_output('\x1b]2;' .. line .. '\x1b\\')
+        end
+      end),
+    },
+  },
+}
+
 return config
